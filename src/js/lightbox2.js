3b033299ddf5c97e1070cad1d5a221dd1ee90ea2
klopez <klopez@wayfair.com>
2023-01-03 00:40:40 -0500

    added custom span for prediction prompts, tweak8

--- src/js/lightbox2.js
+++ src/js/lightbox2.js
@@ -115,18 +115,6 @@
       <div id="lightboxOverlay" tabindex="-1" class="lightboxOverlay"></div>
       <div id="lightbox" tabindex="-1" class="lightbox">
         <div class="lightbox-container">
-          <div class="lb-outerContainer">
-            <div class="lb-container">
-              <img class="lb-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt=""/>
-              <div class="lb-nav">
-                <a class="lb-prev" aria-label="Previous image" href="" ></a>
-                <a class="lb-next" aria-label="Next image" href="" ></a>
-              </div>
-              <div class="lb-loader">
-                <a class="lb-cancel"></a>
-              </div>
-            </div>
-          </div>
           <div class="lb-dataContainer">
             <div class="lb-data">
               <div class="lb-details">
@@ -136,6 +124,18 @@
               </div>
               <div class="lb-closeContainer">
                 <a class="lb-close"></a>
+              </div>
+            </div>
+          </div>
+          <div class="lb-outerContainer">
+            <div class="lb-container">
+              <img class="lb-image" src="data:image/gif;base64,R0lGODlhAQABAIAAAP///wAAACH5BAEAAAAALAAAAAABAAEAAAICRAEAOw==" alt=""/>
+              <div class="lb-nav">
+                <a class="lb-prev" aria-label="Previous image" href="" ></a>
+                <a class="lb-next" aria-label="Next image" href="" ></a>
+              </div>
+              <div class="lb-loader">
+                <a class="lb-cancel"></a>
               </div>
             </div>
           </div>
