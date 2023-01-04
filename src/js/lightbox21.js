855085cbe175955f86a6e92142f1b56e17366277
klopez <klopez@wayfair.com>
2023-01-04 04:04:08 -0500

    added custom span for prediction prompts, tweak35

--- src/js/lightbox21.js
+++ src/js/lightbox21.js
@@ -462,6 +462,8 @@
       var window_width = $(window).width();
       var scroll_offset  = $(window).scrollTop();
       let dataContainer_height = self.$lightbox.find('.lb-dataContainer').height();
+      let lb_prompt_content = self.$lightbox.find('lb-prompt').html();
+      console.log('lb_prompt_content is: ', lb_prompt_content);
       console.log('in postresize, dataContainer_height is: ', dataContainer_height);
 
       var view_offset = window_height/2 - (newHeight)/2;
