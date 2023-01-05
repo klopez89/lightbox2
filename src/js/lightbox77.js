66aa969850bc10b3323ea19aaf1b8a08806eb5bc
klopez <klopez@wayfair.com>
2023-01-05 05:47:09 -0500

    added custom span for prediction prompts, tweak90

--- src/js/lightbox77.js
+++ src/js/lightbox77.js
@@ -460,17 +460,6 @@
         view_offset = window_height/2 - (newHeight + dataContainer_height + 30)/2;;
       }
 
-      var window_height = window.innerHeight;
-      var window_width = $(window).width();
-      var scroll_offset  = $(window).scrollTop();
-
-      let dataContainer_height = self.$lightbox.find('.lb-dataContainer').height();
-      var view_offset = window_height/2 - (newHeight)/2;
-
-      if (window_width <= 1200) {
-        view_offset = window_height/2 - (newHeight + dataContainer_height + 30)/2;;
-      }
-
       var top_distance = scroll_offset + view_offset;
       console.log(`The top distance for lightbox location is: `, top_distance);
       self.$lightbox.css('top', top_distance+'px');
