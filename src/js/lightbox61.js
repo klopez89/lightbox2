078bd046b38532885146ab7fcd4227dc2be692bb
klopez <klopez@wayfair.com>
2023-01-05 04:24:42 -0500

    added custom span for prediction prompts, tweak74

--- src/js/lightbox61.js
+++ src/js/lightbox61.js
@@ -289,8 +289,8 @@
     }
 
     // Position Lightbox
-    window_innerHeight = $(window).height();
-    var top  = $window.scrollTop() + window_innerHeight/2;//this.options.positionFromTop;
+    window_innerHeight = $(window).innerHeight;
+    var top  = $window.scrollTop() + (window_innerHeight - 250) /2;//this.options.positionFromTop;
     var left = $window.scrollLeft();
     this.$lightbox.css({
       top: top + 'px',
