78bd8e3caceea9415f4830f730243a7b1a7e0603
klopez <klopez@wayfair.com>
2023-01-05 16:44:30 -0500

    tweak22

--- src/js/lightbox_a12.js
+++ src/js/lightbox_a12.js
@@ -445,7 +445,7 @@
 
 
       var scroll_offset  = $(window).scrollTop();
-      self.$lightbox.css('top', `calc(50% + ${scroll_offset}px) + env(safe-area-inset-bottom)`);
+      self.$lightbox.css('top', `calc(50% + ${scroll_offset}px) - env(safe-area-inset-bottom)`);
 
       // Set focus on one of the two root nodes so keyboard events are captured.
       // self.$overlay.focus(); // enabling this caused a jump when i was at the bottom of the body on iphone, so removed it
