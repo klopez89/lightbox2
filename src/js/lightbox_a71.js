acad561f8c014184ce1039b1c6404811b8ba1b7c
klopez <klopez@wayfair.com>
2023-01-09 20:46:37 -0500

    getting the hidden details slide up for mobile working, tweak48

--- src/js/lightbox_a71.js
+++ src/js/lightbox_a71.js
@@ -52,7 +52,7 @@
     resizeDuration: 700,
     showImageNumberLabel: true,
     wrapAround: false,
-    disableScrolling: false,
+    disableScrolling: true,
     /*
     Sanitize Title
     If the caption data is trusted, for example you are hardcoding it in, then leave this to false.
